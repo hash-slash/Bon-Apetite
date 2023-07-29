3c82900c4b11cda9dbc14eded8cfb78271761633
Ashesh Saha <ashesh2001@gmail.com>
2023-07-29 23:46:02 +0530

    new edits 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/Card.js 	new file:   src/components/Carousel.js 	modified:   src/components/Footer.js 	modified:   src/index.css 	modified:   src/screens/Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Card from "../components/Card";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
     return (
@@ -8,18 +10,9 @@ export default function Home() {
             <div>
                 <Navbar />
             </div>
-            <div>
-                <div className="card mt = 3" style={{"width": "18rem", "maxHeight": "360px"}}>
-                    <img src="..." className="card-img-top" alt="..."></img>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is imp text</p>
-                            <div className="container w= 100">
-                                <select className="m=2 h=100 w=100 bg-success">
-                                    <option className=""></option>
-                                </select>
-                            </div>
-                </div>
+            <div><Carousel/></div>
+            <div className="m-3">
+                <Card/>
             </div>
             <div>
                 <Footer />
